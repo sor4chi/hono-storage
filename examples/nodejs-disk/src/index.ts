1b4f59ca725ea769969a2a59cda0122a7721b87a
sor4chi <sorakawa0218@gmail.com>
2023-09-25 15:40:25 +0900

    feat: add nodejs-disk example

---
+++ examples/nodejs-disk/src/index.ts
@@ -0,0 +1,14 @@
+import { join } from "path";
+
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { HonoStorage } from "hono-storage-for-nodejs-disk";
+
+const app = new Hono();
+const storage = new HonoStorage({
+  dest: join(__dirname, "tmp"),
+});
+
+app.post("/", storage.single("file"), (c) => c.text("Hello Hono!"));
+
+serve(app);
