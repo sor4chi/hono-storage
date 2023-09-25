663a1cc3acd8e6887df8de35cb544a8031732a9e
sor4chi <sorakawa0218@gmail.com>
2023-09-25 14:54:12 +0900

    feat: implement single and multipe storage handler in hono

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,19 +1,41 @@
-import { Context } from "hono";
+import { MiddlewareHandler } from "hono";
 
-type HonoStorageOptionsRequired =
-  | {
-      dest: string;
-    }
-  | {
-      storage: (c: Context) => Promise<void> | void;
-    };
+type HonoStorageOptions = {
+  storage: (files: File[]) => Promise<void> | void;
+};
 
-type HonoStorageOptions = HonoStorageOptionsRequired;
+const isFile = (value: unknown): value is File => {
+  return value instanceof File;
+};
 
 export class HonoStorage {
-  private options: HonoStorageOptions;
+  options: HonoStorageOptions;
 
   constructor(options: HonoStorageOptions) {
     this.options = options;
   }
+
+  single = (key: string): MiddlewareHandler => {
+    return async (c, next) => {
+      const formData = await c.req.parseBody();
+      const file = formData[key];
+      if (isFile(file)) {
+        await this.options.storage([file]);
+      }
+
+      await next();
+    };
+  };
+
+  multiple = (key: string): MiddlewareHandler => {
+    return async (c, next) => {
+      const formData = await c.req.parseBody();
+      const files = formData[key];
+      if (Array.isArray(files) && files.every(isFile)) {
+        await this.options.storage(files as unknown as File[]);
+      }
+
+      await next();
+    };
+  };
 }
