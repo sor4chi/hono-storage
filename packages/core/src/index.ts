b73cc4c74640f35e860e5b6b1719fd3ff713f829
sor4chi <sorakawa0218@gmail.com>
2023-10-04 07:42:18 +0900

    fix: multi-parse logic using parseAllBody

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,11 @@
 import type { MiddlewareHandler, Context } from "hono";
 
 type HonoStorageOptions = {
-  storage: (c: Context, files: File[]) => Promise<void> | void;
+  storage: (c: Context, files: Blob[]) => Promise<void> | void;
 };
 
-const isFile = (value: unknown): value is File => {
-  return value instanceof File;
+const isBlob = (value: unknown): value is Blob => {
+  return value instanceof Blob;
 };
 
 export class HonoStorage {
@@ -17,10 +17,10 @@ export class HonoStorage {
 
   single = (key: string): MiddlewareHandler => {
     return async (c, next) => {
-      const formData = await c.req.parseBody();
+      const formData = await c.req.parseBody({ all: true });
       const file = formData[key];
 
-      if (isFile(file)) {
+      if (isBlob(file)) {
         await this.options.storage(c, [file]);
       }
 
@@ -30,10 +30,12 @@ export class HonoStorage {
 
   multiple = (key: string): MiddlewareHandler => {
     return async (c, next) => {
-      const formData = await c.req.parseBody();
+      const formData = await c.req.parseBody({ all: true });
       const files = formData[key];
-      if (Array.isArray(files) && files.every(isFile)) {
-        await this.options.storage(c, files as unknown as File[]);
+
+      if (Array.isArray(files) && files.some(isBlob)) {
+        const filteredFiles = files.filter(isBlob);
+        await this.options.storage(c, filteredFiles as unknown as Blob[]);
       }
 
       await next();
--- packages/core/tests/index.test.ts
+++ packages/core/tests/index.test.ts
@@ -20,16 +20,20 @@ describe("HonoStorage", () => {
     });
 
     it("can be used as a text file upload middleware", async () => {
+      const storageHandler = vi.fn();
       const storage = new HonoStorage({
-        storage: (c) => {
-          c.res.headers.set("Check-Whether-Storage-Is-Working", "true");
+        storage: (_, files) => {
+          files.forEach(() => {
+            storageHandler();
+          });
         },
       });
       const app = new Hono();
       app.post("/upload", storage.single("file"), (c) => c.text("Hello World"));
 
       const formData = new FormData();
-      const file = new File(["Hello Hono Storage 1\n"], "sample1.txt");
+
+      const file = new Blob(["Hello Hono Storage 1"], { type: "text/plain" });
       formData.append("file", file);
 
       const res = await app.request("http://localhost/upload", {
@@ -38,8 +42,110 @@ describe("HonoStorage", () => {
       });
 
       expect(res.status).toBe(200);
-      expect(res.headers.get("Check-Whether-Storage-Is-Working")).toBe("true");
+      expect(storageHandler).toBeCalledTimes(1);
       expect(await res.text()).toBe("Hello World");
     });
+
+    it("can be through if the file is not a blob", async () => {
+      const storageHandler = vi.fn();
+      const storage = new HonoStorage({
+        storage: () => {
+          storageHandler();
+        },
+      });
+      const app = new Hono();
+      app.post("/upload", storage.single("file"), (c) => c.text("Hello World"));
+
+      const formData = new FormData();
+
+      formData.append("file", "Hello Hono Storage 1");
+
+      const res = await app.request("http://localhost/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      expect(res.status).toBe(200);
+      expect(storageHandler).not.toBeCalled();
+      expect(await res.text()).toBe("Hello World");
+    });
+  });
+
+  describe("multiple", () => {
+    it("can be used as a middleware", async () => {
+      const storage = new HonoStorage({
+        storage: () => {},
+      });
+      const app = new Hono();
+      app.post("/", storage.multiple("file"), (c) => c.text("Hello World"));
+    });
+
+    it("can be used as some text file upload middleware", async () => {
+      const storageHandler = vi.fn();
+      const storage = new HonoStorage({
+        storage: (_, files) => {
+          files.forEach(() => {
+            storageHandler();
+          });
+        },
+      });
+      const app = new Hono();
+      app.post("/upload", storage.multiple("file"), (c) =>
+        c.text("Hello World"),
+      );
+
+      const formData = new FormData();
+
+      formData.append(
+        "file",
+        new Blob(["Hello Hono Storage 1"], {
+          type: "text/plain",
+        }),
+        "sample1.txt",
+      );
+
+      formData.append(
+        "file",
+        new Blob(["Hello Hono Storage 2"], {
+          type: "text/plain",
+        }),
+        "sample2.txt",
+      );
+
+      const res = await app.request("http://localhost/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      expect(res.status).toBe(200);
+      expect(storageHandler).toBeCalledTimes(2);
+      expect(await res.text()).toBe("Hello World");
+    });
+
+    // it("can be filtered if the files is not a blob", async () => {
+    //   const storage = new HonoStorage({
+    //     storage: () => {},
+    //   });
+    //   const app = new Hono();
+    //   app.post("/upload", storage.multiple("file"), (c) =>
+    //     c.text("Hello World"),
+    //   );
+
+    //   const formData = new FormData();
+
+    //   formData.append("file", "Hello Hono Storage 1");
+    //   formData.append(
+    //     "file",
+    //     new Blob(["Hello Hono Storage 2"], { type: "text/plain" }),
+    //   );
+
+    //   const res = await app.request("http://localhost/upload", {
+    //     method: "POST",
+    //     body: formData,
+    //   });
+
+    //   expect(res.status).toBe(200);
+    //   expect(await res.text()).toBe("Hello World");
+    // });
   });
 });
