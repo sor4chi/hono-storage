4f721ba4871c805cb1ad3872cce60e322f2267f0
sor4chi <sorakawa0218@gmail.com>
2023-10-13 02:54:55 +0900

    test(node-disk): mocking fs

--- packages/node-disk/tests/index.test.ts
+++ packages/node-disk/tests/index.test.ts
@@ -1,4 +1,3 @@
-import * as fs from "fs/promises";
 import { join } from "path";
 
 import { createAdaptorServer } from "@hono/node-server";
@@ -16,19 +15,11 @@ describe("HonoDiskStorage", () => {
   });
 
   describe("dest option", () => {
-    afterEach(async () => {
-      try {
-        await fs.access(join(__dirname, "tmp"));
-        await fs.rm(join(__dirname, "tmp"), { recursive: true });
-      } catch (e) {
-        // ignore
-      }
-    });
-
     it("can be used as a file upload middleware", async () => {
       const storage = new HonoDiskStorage({
         dest: join(__dirname, "tmp"),
       });
+      const spyHandleDestStorage = vi.spyOn(storage, "handleDestStorage");
       const app = new Hono();
       app.post("/upload", storage.single("file"), (c) => c.text("Hello World"));
       const server = createAdaptorServer(app);
@@ -37,25 +28,15 @@ describe("HonoDiskStorage", () => {
         .attach("file", join(__dirname, "fixture/sample1.txt"));
       expect(res.status).toBe(200);
       expect(res.text).toBe("Hello World");
-      const files = await fs.readdir(join(__dirname, "tmp"));
-      expect(files).toEqual(["sample1.txt"]);
-      const content = await fs.readFile(join(__dirname, "tmp/sample1.txt"), {
-        encoding: "utf-8",
-      });
-      expect(content).toBe("Hello Hono Storage 1\n");
+      expect(spyHandleDestStorage).toBeCalledWith(
+        expect.objectContaining({
+          name: "sample1.txt",
+        }),
+      );
     });
   });
 
   describe("filename option", () => {
-    afterEach(async () => {
-      try {
-        await fs.access(join(__dirname, "tmp"));
-        await fs.rm(join(__dirname, "tmp"), { recursive: true });
-      } catch (e) {
-        // ignore
-      }
-    });
-
     it("can be used as a file upload middleware", async () => {
       const PREFIX = "prefix-";
       const storage = new HonoDiskStorage({
@@ -63,6 +44,7 @@ describe("HonoDiskStorage", () => {
         filename: (_, file) =>
           `${PREFIX}${file.originalname}.${file.extension}`,
       });
+      const spyHandleDestStorage = vi.spyOn(storage, "handleDestStorage");
       const app = new Hono();
       app.post("/upload", storage.single("file"), (c) => c.text("Hello World"));
       const server = createAdaptorServer(app);
@@ -71,15 +53,11 @@ describe("HonoDiskStorage", () => {
         .attach("file", join(__dirname, "fixture/sample1.txt"));
       expect(res.status).toBe(200);
       expect(res.text).toBe("Hello World");
-      const files = await fs.readdir(join(__dirname, "tmp"));
-      expect(files).toEqual(["prefix-sample1.txt"]);
-      const content = await fs.readFile(
-        join(__dirname, "tmp/prefix-sample1.txt"),
-        {
-          encoding: "utf-8",
-        },
+      expect(spyHandleDestStorage).toBeCalledWith(
+        expect.objectContaining({
+          name: `${PREFIX}sample1.txt`,
+        }),
       );
-      expect(content).toBe("Hello Hono Storage 1\n");
     });
   });
 });
