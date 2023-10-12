a76b9470db6447b3dbbd50e27b150c00b7d2bcfc
sor4chi <sorakawa0218@gmail.com>
2023-10-12 14:20:43 +0900

    chore: update example for filename option

--- examples/nodejs-disk/src/index.ts
+++ examples/nodejs-disk/src/index.ts
@@ -1,12 +1,11 @@
-import { join } from "path";
-
 import { serve } from "@hono/node-server";
+import { HonoDiskStorage } from "@hono-storage/nodejs-disk";
 import { Hono } from "hono";
-import { HonoDiskStorage } from "hono-storage-for-nodejs-disk";
 
 const app = new Hono();
 const storage = new HonoDiskStorage({
-  dest: join(__dirname, "tmp"),
+  dest: "./uploads",
+  filename: (_, file) => `${file.originalname}-${Date.now()}.${file.extension}`,
 });
 
 app.post("/", storage.single("file"), (c) => c.text("OK"));
