dfb1cd81eb588404cd39b8158fe7c1dd81b3d763
sor4chi <sorakawa0218@gmail.com>
2023-10-15 03:13:32 +0900

    feat: implement s3 storage

---
+++ packages/s3/src/index.ts
@@ -0,0 +1,70 @@
+import {
+  S3Client,
+  PutObjectCommand,
+  PutObjectRequest,
+} from "@aws-sdk/client-s3";
+import { HonoStorage, HonoStorageFile } from "@hono-storage/core";
+import { Context } from "hono";
+
+type HSSFunction<T> = (c: Context, file: HonoStorageFile) => T;
+type UploadCustomParams = Omit<
+  PutObjectRequest,
+  "Bucket" | "Key" | "Body" | "ContentType" | "ContentLength"
+>;
+
+export type HonoS3StorageOptions = {
+  key?: HSSFunction<string>;
+  bucket: string | HSSFunction<string>;
+  client: S3Client | HSSFunction<S3Client>;
+  params?: UploadCustomParams;
+};
+
+export class HonoS3Storage extends HonoStorage {
+  key: HSSFunction<string>;
+  bucket: string | HSSFunction<string>;
+  client: S3Client | HSSFunction<S3Client>;
+  params?: UploadCustomParams;
+
+  constructor(options: HonoS3StorageOptions) {
+    super({
+      storage: async (c, files) => {
+        await Promise.all(
+          files.map(async (file) => {
+            await this.upload(c, file);
+          }),
+        );
+      },
+    });
+
+    this.key = options.key ?? ((_, file) => file.name);
+    this.bucket = options.bucket;
+    this.client = options.client;
+    this.params = options.params;
+  }
+
+  async upload(c: Context, file: File) {
+    const key = this.key(c, new HonoStorageFile(file));
+    const bucket =
+      typeof this.bucket === "function"
+        ? this.bucket(c, new HonoStorageFile(file))
+        : this.bucket;
+
+    // for nodejs
+    const isBufferExists = typeof Buffer !== "undefined";
+
+    const command = new PutObjectCommand({
+      Bucket: bucket,
+      Key: key,
+      Body: isBufferExists ? Buffer.from(await file.arrayBuffer()) : file,
+      ContentType: file.type,
+      ContentLength: file.size,
+      ...this.params,
+    });
+
+    if (typeof this.client === "function") {
+      await this.client(c, new HonoStorageFile(file)).send(command);
+    } else {
+      await this.client.send(command);
+    }
+  }
+}
