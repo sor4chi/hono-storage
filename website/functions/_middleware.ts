abfceccfee1740e5f7e876b01a0aa7910d9cb85f
seiei-n <solehamugoiyo@gmail.com>
2023-11-05 23:00:07 +0900

    fix: redirect logic

--- website/functions/_middleware.ts
+++ website/functions/_middleware.ts
@@ -1,29 +1,36 @@
-const getBrowserLanguage = () => {
-  if (typeof window === "undefined") return "en";
-  const lang = window.navigator.language;
-  if (lang.indexOf("ja") === 0) return "ja";
-  if (lang.indexOf("en") === 0) return "en";
-  return "ja";
+const SUPPORTED_LANGUAGES = ["en", "ja"];
+
+const getBrowserLanguageFromHeader = (req: Request) => {
+  const acceptLanguage = req.headers.get("Accept-Language");
+  if (!acceptLanguage) return;
+  const lang = acceptLanguage.split(",")[0].split("-")[0];
+  if (!SUPPORTED_LANGUAGES.includes(lang)) return;
+  return lang;
+}
+
+const isi18nPath = (path: string) => {
+  const parts = path.split("/");
+  return SUPPORTED_LANGUAGES.includes(parts[1]);
 };
+
+const geti18nPath = (path: string, lang: string) => {
+  const parts = path.split("/").filter((part) => part);
+  return [lang, ...parts].join("/");
+}
 
 export const onRequest: PagesFunction = async (context) => {
   const { request, next } = context;
-  const lang = getBrowserLanguage();
-  if (lang === "en") {
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: `/en${request.url}`,
-      },
-    });
-  }
-  if (lang === "ja") {
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: `/ja${request.url}`,
-      },
-    });
-  }
-  return next();
+  const url = new URL(request.url);
+  if (isi18nPath(url.pathname)) return await next();
+  const lang = getBrowserLanguageFromHeader(request);
+  if (!lang) return await next();
+  const i18nPath = geti18nPath(url.pathname, lang);
+  if (!i18nPath) return await next();
+  const i18nedUrl = new URL(i18nPath, url.origin).toString();
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: i18nedUrl,
+    },
+  });
 };
